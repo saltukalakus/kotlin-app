7c6179a763fa049ea38658c78050a2d8614da330
bruno.krebs <krebs.bruno@gmail.com>
2017-06-22 09:53:33 -0300

    Listing to do items

--- app/src/main/java/com/auth0/samples/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/auth0/samples/kotlinapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.auth0.samples.kotlinapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // setting up a Volley RequestQueue
+        val queue = Volley.newRequestQueue(this)
+
+        // getting a reference for the ListView
+        val listToDo = findViewById(R.id.list_todo) as ListView
+
+        // passing the activity, the queue and the ListView to the function
+        // that consumes the RESTful endpoint
+        getItems(this, queue, listToDo)
     }
 }
